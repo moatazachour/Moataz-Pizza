ce762b096bd54d4176f104bf13d8563427287b1d
moatazachour <moatazachour84@gmail.com>
2026-04-25 19:25:00 +0100

    Add image to pizza component, add css styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,21 @@ const App = () => {
   return (
     <div>
       <h1>Moataz Pizza - Order Now</h1>
-      <Pizza name="Margharita" description="Sauce and Mozzarella" />
-      <Pizza name="Neptune" description="Sauce, Mozzarella and Tuna" />
-      <Pizza name="Queen" description="Sauce, Mozzarella, Ham and Mushrooms" />
+      <Pizza
+        name="Margharita"
+        description="Sauce and Mozzarella"
+        image={"/public/pizzas/sicilian.webp"}
+      />
+      <Pizza
+        name="Neptune"
+        description="Sauce, Mozzarella and Tuna"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Queen"
+        description="Sauce, Mozzarella, Ham and Mushrooms"
+        image={"/public/pizzas/big_meat.webp"}
+      />
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
       },
+      "/public": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
     },
   },
   plugins: [react()],
