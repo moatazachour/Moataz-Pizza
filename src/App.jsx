0edc6d115a442cb16246e9a06eea8e71881d1eb4
moatazachour <moatazachour84@gmail.com>
2026-06-08 00:16:00 +0100

    Create the order component, and add it to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Moataz Pizza - Order Now</h1>
-      <Pizza
-        name="Margharita"
-        description="Sauce and Mozzarella"
-        image={"/public/pizzas/sicilian.webp"}
-      />
-      <Pizza
-        name="Neptune"
-        description="Sauce, Mozzarella and Tuna"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Queen"
-        description="Sauce, Mozzarella, Ham and Mushrooms"
-        image={"/public/pizzas/big_meat.webp"}
-      />
+      <Order />
     </div>
   );
 };
