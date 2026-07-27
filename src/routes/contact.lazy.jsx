a96b1731024c94ae607324f79563b5c1e9880f21
moatazachour <moatazachour84@gmail.com>
2026-07-27 19:39:35 +0100

    Add Contact route and form for user inquiries

---
+++ src/routes/contact.lazy.jsx
@@ -0,0 +1,41 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useMutation } from "@tanstack/react-query";
+import postContact from "../api/postContact";
+
+export const Route = createLazyFileRoute("/contact")({
+  component: ContactRoute,
+});
+
+function ContactRoute() {
+  const mutation = useMutation({
+    mutationFn: function (e) {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      return postContact(
+        formData.get("name"),
+        formData.get("email"),
+        formData.get("message"),
+      );
+    },
+  });
+
+  if (mutation.isError) {
+    return <h1>Lol, nooo!</h1>;
+  }
+
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      {mutation.isSuccess ? (
+        <h3>Submitted:</h3>
+      ) : (
+        <form onSubmit={mutation.mutate}>
+          <input name="name" placeholder="Name" type="text" />
+          <input name="email" placeholder="Email" type="email" />
+          <textarea name="message" placeholder="Message"></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
+}
